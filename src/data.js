12dc8c8ef456b611957f60a823499035b7fd5aec
Grefel <felnable@gmail.com>
2022-05-04 12:19:31 -0400

    re-arrange project list from recent to oldest

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 const Data = [
   {
-    id: 1,
-    image: "./images/weather-app.png",
-    name: "weather app",
-    github: "https://github.com/grefelnable/weather-app-grefel",
-    demo: "https://weather-app-grefel.netlify.app/",
+    id: 3,
+    image: "./images/amazon-clone.png",
+    name: "Amazon Clone app",
+    github: "https://github.com/grefelnable/amazon-clone-grefel",
+    demo: "https://amazon-clone-grefel.netlify.app/",
   },
   {
     id: 2,
@@ -14,11 +14,11 @@ const Data = [
     demo: "https://clock-app-grefel.netlify.app/",
   },
   {
-    id: 3,
-    image: "./images/amazon-clone.png",
-    name: "Amazon Clone app",
-    github: "https://github.com/grefelnable/amazon-clone-grefel",
-    demo: "https://amazon-clone-grefel.netlify.app/",
+    id: 1,
+    image: "./images/weather-app.png",
+    name: "weather app",
+    github: "https://github.com/grefelnable/weather-app-grefel",
+    demo: "https://weather-app-grefel.netlify.app/",
   },
 ]
 
